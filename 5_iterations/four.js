25d5d21a669ce850b2d973b8c4327ef927f8ba1c
Kalesh Patil <159576620+kalesh-94@users.noreply.github.com>
2025-03-18 09:07:12 +0000

    chaining

---
+++ 5_iterations/four.js
@@ -0,0 +1,8 @@
+const Codingnums = [1,2,4,5,6,7,8,9,21,22,33,44,55,66]
+
+const mynums = Codingnums.filter((num)=>num>5)
+                                            .map((num)=>num*10)
+                                            .map((num)=>num-1)  
+                                            //// this is called as chaining (function chaining)
+console.log(mynums);
+
---
+++ 5_iterations/three.js
@@ -0,0 +1,14 @@
+const Nums = [1,2,3,4,5,6,8]
+
+let newvr = Nums.filter((n)=>{
+    return n>5
+})
+
+console.log(newvr);
+
+// newvr = Nums.map((item)=>{
+//     return item+10         ==========>>> return use because of {} scope
+
+// }) 
+console.log(newvr);
+
