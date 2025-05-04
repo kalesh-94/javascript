21dc9846d240fc0d4e3fabbe9e260635930d4145
Kalesh Patil <159576620+kalesh-94@users.noreply.github.com>
2025-05-04 22:04:26 +0000

    projects

---
+++ 7_Events/events1.js
@@ -0,0 +1,6 @@
+const sparrow = document.getElementById("sparrow")
+
+sparrow.addEventListener("click", function(e){
+    alert("sparrow is clicked brooooooo !!!!!!!");
+    console.log("sparrow is clicked");
+})
