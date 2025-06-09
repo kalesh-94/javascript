f88b2558ffe8a5db9736e1a53fac6ea4b37a4e3d
Kalesh Patil <159576620+kalesh-94@users.noreply.github.com>
2025-06-09 19:37:38 +0000

    objectsjs

---
+++ 10_objects/objects.js
@@ -0,0 +1,31 @@
+console.log(Math.PI);
+Math.PI = 5
+const descriptor = Object.getOwnPropertyDescriptor(Math, "PI")
+console.log(descriptor);
+
+
+
+
+
+const user = {
+    name:'kalesh',
+    id:1331,
+    party:"yes"
+}
+
+for (const [key,value] of Object.entries(user)) {
+    console.log(`${key} and ${value}`);
+}
+
+
+Object.defineProperty(user, "name", {
+    writable:false,
+    enumerable:false,
+
+})
+
+console.log(Object.getOwnPropertyDescriptor(user, "name"));
+
+for (const [key,value] of Object.entries(user)) { /////iterable on name will not work , only id and party will iterable
+    console.log(`${key} and ${value}`);
+}
